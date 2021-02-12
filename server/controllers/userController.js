79e4bac24761fbf89738719629cb918f239bc7f3
Andrew Miller <206alm@gmail.com>
2021-02-12 13:36:35 -0800

    user model crud done except update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,13 +6,74 @@ module.exports = {
             .then(users => {
                 res.send(users);
               })
-              .catch(err => {
+            .catch(err => {
                 res.status(500).send({
-                  message:
+                    message:
                     err.message || 'Some error occurred while retrieving users'
                 });
+        });
+    },
+    findOne: (req, res) => {
+        models.User.findOne(
+            { where: {
+                email: req.body.email
+            }
+        })
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || 'Some error occurred while retrieving user'
             });
-        // console.log('this is the first route test -- GET /api/user\nwill be get all users')
-        // res.send('this is the first route test -- GET /api/user\nwill be get all users') 
+        })
+    },
+    create: (req, res ) => {
+        if(!req.body.username || !req.body.password) {
+            res.status(400).send({ error:true, message: 'Please provide name/pass' });
+        }
+        models.User.findOne( { where: { username: req.body.username }})
+        .then(user => {
+            if (user) {
+                console.log(`user found, choose a different name ${user}`)
+                return res.json({
+                    success: false,
+                    username: null,
+                    error: `Username ${req.body.username} has already been taken.`
+                })
+            }
+            else {
+                console.log(`no user found, ready to create`)
+                models.User.create(req.body).then(user => {
+                    return res.json({
+                        success: true,
+                        username: user.username,
+                    })
+                })
+                .catch(error => {
+                        console.log(error);
+                })
+            }
+        })
+        .catch (err => {
+            console.log(err)
+        })
+    },
+    update: (req, res) => {
+        // update not yet necessary
+    },
+    destroy: (req, res) => {
+        models.User.destroy({
+            where: {
+                id: req.params.userId
+            }
+        })
+        .then(user => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 }
--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-// Matches with "/api/box"
-router.route("/")
+router.route('/')
   .get(userController.findAll)
+  .post(userController.create)
 
+router.route('/:userId')
+  .get(userController.findOne)
+  // .put(user.update_a_user)
+  .delete(userController.destroy);
 module.exports = router;
