b4a632baa42ac7be944380c7610678c936168dc5
Andrew Miller <jazznerd206@icloud.com>
2021-02-16 07:07:45 -0800

    begin login flow

---
+++ frontend/src/components/Box/Box.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Box() {
+    return (
+        <div>
+            BOX PAGE
+        </div>
+    )
+}
+
+export default Box
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -3,17 +3,36 @@ import './style.css';
 
 function Login() {
 
-    const [ form, setForm ] = useState('login')
+    const [ form, setForm ] = useState('login');
+    const [ name, setName ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ checkPassword, setCheckPassword ] = useState('');
+
+    const submitRegister = () => {
+        console.log(`submit login for: ${name}, ${password}`)
+    }
 
     if (form === 'login') {
         return (
             <div className="login-wrapper">
                 <div className="login-slide">
                     <label>Name:</label>
-                    <input></input>
+                    <input
+                        type="text"
+                        value={name}
+                        id="name"
+                        onChange={(e) => setName(e.target.value)}
+                    ></input>
                     <label>Password:</label>
-                    <input></input>
-                    <button>Log In</button>
+                    <input
+                        type="password"
+                        value={password}
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                    <button
+                        onClick={() => submitRegister()}
+                    >Log In</button>
                 </div>
                 <div className="login-slide">
                     <button onClick={() => setForm('register')}>Sign Up</button>
@@ -29,12 +48,29 @@ function Login() {
                 </div>
                 <div className="login-slide">
                     <label>Name:</label>
-                    <input></input>
+                    <input
+                        type="text"
+                        value={name}
+                        id="name"
+                        onChange={(e) => setName(e.target.value)}
+                    ></input>
                     <label>Password:</label>
-                    <input></input>
-                    <label> Retype Password:</label>
-                    <input></input>
-                    <button>Register</button>
+                    <input
+                        type="password"
+                        value={password}
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                    <label>Check Password:</label>
+                    <input
+                        type="checkPassword"
+                        value={checkPassword}
+                        id="checkPassword"
+                        onChange={(e) => setCheckPassword(e.target.value)}
+                    ></input>
+                    <button
+                        onClick={() => console.log(`submit register for: ${name}, ${password}, ${checkPassword}`)}
+                    >Register</button>
                 </div>
             </div>
         )
